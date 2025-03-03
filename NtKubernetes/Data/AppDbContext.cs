d7130a4dfdeb6342d7515e89e47b2e6265b04ceb
Admin <Admin@DESKTOP-EVDT7PO>
2025-03-03 02:49:32 -0800

    Add project files.

---
+++ NtKubernetes/Data/AppDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using NtKubernetes.Model;
+
+namespace NtKubernetes.Data;
+
+public class AppDbContext: DbContext
+{
+    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+{
+}
+    public DbSet<Producto> Produtos { get; set; }
+
+}
---
+++ NtKubernetes/Model/Producto.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace NtKubernetes.Model;
+
+public class Producto
+{
+    [Key]
+    [Required]
+    public int Id { get; set; }
+    public string? Nome { get; set; }
+    public string? Drescicao { get; set; }
+    [Required]
+    [Column(TypeName = "decimal(18,4)")]
+    public decimal Preco { get; set; }
+    public string? Imagem { get; set; }
+    public DateTime? DataCriacao { get; set; }
+}
