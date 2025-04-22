105ec65629a56689dc1295fbf449403599b3786a
LEOMAZENDA2 <leonildo.mazenda@hotmail.com>
2025-04-22 19:59:32 +0100

    Adicionado todos os pacotes

--- _backend/NetKubernetes/Program.cs
+++ _backend/NetKubernetes/Program.cs
@@ -16,9 +16,8 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
-//app.UseHttpsRedirection();
+app.UseHttpsRedirection();
 
-app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllers();
