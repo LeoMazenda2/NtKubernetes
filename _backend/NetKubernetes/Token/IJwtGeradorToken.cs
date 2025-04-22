20f6ec4b504719f3c679c73acd236985f0ce3001
LEOMAZENDA2 <leonildo.mazenda@hotmail.com>
2025-04-22 21:02:18 +0100

    Metodo para Criar token feito

---
+++ _backend/NetKubernetes/Token/IJwtGeradorToken.cs
@@ -0,0 +1,8 @@
+﻿using NetKubernetes.Models;
+
+namespace NetKubernetes.Token;
+
+public interface IJwtGeradorToken{
+    string GearToken(Usuario usuario);
+
+}
---
+++ _backend/NetKubernetes/Token/JwtGeradorToken.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.IdentityModel.Tokens;
+using NetKubernetes.Models;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+
+namespace NetKubernetes.Token;
+
+public class JwtGeradorToken : IJwtGeradorToken
+{
+    public string GearToken(Usuario usuario)
+    {
+        var claims = new List<Claim> {
+           new Claim(JwtRegisteredClaimNames.NameId, usuario.UserName!),
+           new Claim("userId", usuario.Id!),
+           new Claim("email", usuario.Email!),
+       };
+
+        var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes("Minha palavra Secreta"));
+        var credenciais = new SigningCredentials(key, SecurityAlgorithms.HmacSha512Signature);
+
+        var tokenDescripton = new SecurityTokenDescriptor
+        {
+            Subject = new ClaimsIdentity(claims),
+            Expires = DateTime.Now.AddDays(7),
+            SigningCredentials = credenciais
+        };
+
+        var tokenHandler = new JwtSecurityTokenHandler();
+        var token = tokenHandler.CreateToken(tokenDescripton);
+
+        return tokenHandler.WriteToken(token);
+    }
+}
